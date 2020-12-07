4d0216ce2070c9754b4973c38d4c63c429affefb
Chyree Mozer <Chyree.mozerm@student.rocva.nl>
2020-12-07 11:44:26 +0100

    taak7 af

--- 05 - Javascript/taak07 - Intelligente DOM/website/script.js
+++ 05 - Javascript/taak07 - Intelligente DOM/website/script.js
@@ -7,3 +7,9 @@ console.log(window.document);
 
 
 // document.querySelector("#poster1");
+var elem = document.querySelector('#poster1');
+elem.style.backgroundColor = '#e5e5e5';
+var elem = document.querySelector('#poster2');
+elem.style.backgroundColor = '#e5e5e5';
+var elem = document.querySelector('#poster3');
+elem.style.backgroundColor = '#e5e5e5';
